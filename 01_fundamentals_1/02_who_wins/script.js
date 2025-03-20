04ec6259518168f227db513475c2e35391e280da
Ivan Bensa <ivanb.webdev@proton.me>
2025-03-20 21:05:30 +0100

    Create script.js

---
+++ 01_fundamentals_1/02_who_wins/script.js
@@ -0,0 +1,28 @@
+/*
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if(scoreDolphins > scoreKoalas){
+    console.log("Dolphins win the trophy🏆");
+} else if(scoreDolphins < scoreKoalas) {
+    console.log("Koalas win the trophy🏆");
+} else {
+    console.log("Both win the trophy🏆🏆");
+};
+    */
+
+
+const scoreDolphins = (97 + 112 + 90) / 3;
+const scoreKoalas = (109 + 95 + 90) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if(scoreDolphins > scoreKoalas && scoreDolphins >= 100){
+    console.log("Dolphins win the trophy🏆");
+} else if(scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+    console.log("Koalas win the trophy🏆");
+} else if(scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas) {
+    console.log("Both win the trophy🏆🏆");
+} else {
+    console.log("No one wins the trophy 😭");
+};
